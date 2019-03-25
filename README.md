91419aa210e80632824ce5fe3c9732eef6d9f3ff
Daniel Bank <daniel.p.bank@gmail.com>
2019-03-24 23:50:11 -0700

    Adding SVM Classification

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
-# rust-ml-examples
-
-Rust ML Examples
-
-# Talking Points
+# Rust ML Examples
 
 ## Rusty Machine
 
 -   [Are We Learning Yet](http://www.arewelearningyet.com/)
+-   [Rusty Machine](https://github.com/AtheMathmo/rusty-machine/)
 -   [Rusty Machine Model traits](http://athemathmo.github.io/2016/07/28/rusty-machine-talk.html#/10)
 -   [Rusty Machine vs Rust Sim](https://github.com/AtheMathmo/rusty-machine/issues/199)
 
 ## CSV
 
 -   [CSV Tutorial](https://docs.rs/csv/1.0.5/csv/tutorial/index.html)
+-   [Error Handling in Rust](https://blog.burntsushi.net/rust-error-handling/)
 
 ## Linear Regression
 
 -   [Linear Regression](https://newonlinecourses.science.psu.edu/stat501/node/382/)
 
--   Python and Rust Code
+## SVM Classification
+
+-   [Support Vector Machines](https://scikit-learn.org/stable/modules/svm.html)
+
+## Challenges
+
+-   Iterating over records from a CSV
+-   Concatenating one vector to another
+-   Understanding crate-specific types
 
 # Crates
 
@@ -26,11 +32,3 @@ Rust ML Examples
 -   [csv](https://crates.io/crates/csv)
 -   [serde](https://crates.io/crates/serde)
 -   [serde_derive](https://crates.io/crates/serde_derive)
-
-# Links
-
--   [Are We Learning Yet](http://www.arewelearningyet.com/)
--   [Rusty Machine Slides](http://athemathmo.github.io/2016/07/28/rusty-machine-talk.html)
--   [Rusty Machine](https://github.com/AtheMathmo/rusty-machine/)
--   [Linear Regression](https://newonlinecourses.science.psu.edu/stat501/node/382/)
--   [Error Handling in Rust](https://blog.burntsushi.net/rust-error-handling/)
---
+++ classification/src/main.rs
@@ -0,0 +1,84 @@
+extern crate csv;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate rusty_machine;
+
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
+
+use rusty_machine::learning::svm::SVM;
+use rusty_machine::learning::SupModel;
+use rusty_machine::linalg::Matrix;
+use rusty_machine::linalg::Vector;
+use rusty_machine::learning::toolkit::kernel;
+use rusty_machine::learning::toolkit::kernel::Polynomial;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Record {
+    x1: f64,
+    x2: f64,
+    y: f64
+}
+
+fn run() -> Result<(), Box<Error>> {
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+
+    let mut x1_vec: Vec<f64> = Vec::new();
+    let mut x2_vec: Vec<f64> = Vec::new();
+    let mut targets_vec: Vec<f64> = Vec::new();
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(file);
+
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        x1_vec.push(record.x1);
+        x2_vec.push(record.x2);
+        if record.y == 0.0 {
+            targets_vec.push(-1.0);
+        } else {
+            targets_vec.push(1.0);
+        }
+    }
+
+    let x_vec = [&x1_vec[..], &x2_vec[..]].concat();
+    let inputs = Matrix::new(x1_vec.len(), 2, x_vec);
+    let targets = Vector::new(targets_vec);
+
+    let model = train_model(inputs, targets);
+
+    let new_point = Matrix::new(1, 2, vec![0.0, 0.0]);
+    let output = model.predict(&new_point).unwrap();
+    println!("Prediction for {}: {} ", new_point[[0,0]], output[0]);
+
+    Ok(())
+}
+
+fn train_model(inputs: Matrix<f64>, targets: Vector<f64>) -> SVM<Polynomial> {
+    // Constructs a new polynomial with alpha = 1, c = 0, d = 2.
+    let ker = kernel::Polynomial::new(1.0, 0.0, 2.0);
+    let mut svm_mod = SVM::new(ker,0.3);
+    svm_mod.train(&inputs, &targets).unwrap();
+    svm_mod
+}
+
+fn get_first_arg() -> Result<OsString, Box<Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
